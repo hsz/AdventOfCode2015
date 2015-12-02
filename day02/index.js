396eb4200b809925cbba015c8cb579ba920bbfa3
hsz <jakub@chrzanowski.info>
2015-12-02 10:04:51 +0100

    day 02

---
+++ day02/index.js
@@ -0,0 +1,27 @@
+var _    = require('underscore')
+  , fs   = require('fs')
+  , path = require('path')
+  ;
+
+module.exports = function () {
+
+  var input = fs.readFileSync(path.join(__dirname, './input.txt'), 'utf8').split('\n').map(function (v) {
+    return _.sortBy(v.split('x').map(_.partial(parseInt, _, 10)));
+  });
+
+  var part = {
+
+    one: _.reduce(input, function (sum, v) {
+      return sum + 3 * v[0] * v[1] + 2 * v[0] * v[2] + 2 * v[1] * v[2];
+    }, 0),
+
+    two: _.reduce(input, function (sum, v) {
+      return sum + 2 * v[0] + 2 * v[1] + v[0] * v[1] * v[2];
+    }, 0)
+
+  };
+
+  console.log('part one:', part.one);
+  console.log('part two:', part.two);
+
+};
