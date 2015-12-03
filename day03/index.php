0592b152d0d21de4817ddbca76be2750ea1a2095
hsz <jakub@chrzanowski.info>
2015-12-03 14:22:06 +0100

    day 03 part 2 (php)

--- day03/index.php
+++ day03/index.php
@@ -10,8 +10,10 @@ return [
         return $sum + (!isset($a["$x~$y"]) && $a["$x~$y"] = true);
     }, 1),
 
-    two => array_reduce($input, function ($sum, $v) {
-        return null;
-    }, 0),
+    two => ($a = 0) + array_reduce($input, function ($sum, $v) use (&$x, &$y, &$a, &$r) {
+        !$a ? (($x = $y = [($r = 0), 0]) && ($a = ['0~0' => true])) : $r = !$r;
+        $v === '>' ? ++$x[$r] : ($v === '<' ? --$x[$r] : ($v === '^' ? ++$y[$r] : --$y[$r]));
+        return $sum + (!isset($a["$x[$r]~$y[$r]"]) && $a["$x[$r]~$y[$r]"] = true);
+    }, 1),
 
 ];
