021d35428977e24a65963fdbd3574fad31e52b45
hsz <jakub@chrzanowski.info>
2015-12-03 14:33:29 +0100

    day 03 part 2 (php) cleanup

--- day03/index.php
+++ day03/index.php
@@ -5,15 +5,13 @@ $input = str_split(file('input.txt')[0]);
 return [
 
     one => array_reduce($input, function ($sum, $v) use (&$x, &$y, &$a) {
-        !$a && (($x = $y = 0) || ($a = ['0~0' => true]));
         $v === '>' ? ++$x : ($v === '<' ? --$x : ($v === '^' ? ++$y : --$y));
         return $sum + (!isset($a["$x~$y"]) && $a["$x~$y"] = true);
-    }, 1),
+    }, ($x = $y = 0) && $a = ['0~0' => true]),
 
-    two => ($a = 0) + array_reduce($input, function ($sum, $v) use (&$x, &$y, &$a, &$r) {
-        !$a ? (($x = $y = [($r = 0), 0]) && ($a = ['0~0' => true])) : $r = !$r;
-        $v === '>' ? ++$x[$r] : ($v === '<' ? --$x[$r] : ($v === '^' ? ++$y[$r] : --$y[$r]));
+    two => array_reduce($input, function ($sum, $v) use (&$x, &$y, &$a, &$r) {
+        ($r = !$r); $v === '>' ? ++$x[$r] : ($v === '<' ? --$x[$r] : ($v === '^' ? ++$y[$r] : --$y[$r]));
         return $sum + (!isset($a["$x[$r]~$y[$r]"]) && $a["$x[$r]~$y[$r]"] = true);
-    }, 1),
+    }, ($x = $y = [($r = 0), 0]) && ($a = ['0~0' => true])),
 
 ];
