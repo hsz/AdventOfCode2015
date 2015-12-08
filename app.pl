0959d12d52dee5b0d238134b494f8cb8be770724
hsz <jakub@chrzanowski.info>
2015-12-08 11:35:55 +0100

    perl scaffold

---
+++ app.pl
@@ -0,0 +1,16 @@
+use strict;
+use Cwd;
+use Term::ANSIColor qw(:constants);
+
+my $day = '01';
+
+chdir 'day' . $day;
+require 'index.pl';
+
+print GREEN, ON_BLACK, "    Advent of Code    ", RESET, "\n";
+print GREEN, ON_BLACK, "  ------- ", RED, $day, GREEN, " -------  ", RESET, "\n";
+print GREEN, ON_BLACK, "======================", RESET, "\n";
+print "\n";
+print " Part one: ", one(), "\n";
+print " Part two: ", two(), "\n";
+print "\n";
