94c1ba156df500389ac9b46f4f2833612df15741
hsz <jakub@chrzanowski.info>
2015-12-14 21:51:07 +0100

    day 13 (nodejs) cleanup

--- day13/index.js
+++ day13/index.js
@@ -9,23 +9,20 @@ module.exports = function () {
         _.contains(keys, (v = v.replace('.', '').split(' '))[0]) || keys.push(v[0]);
         return map[v[0] + v[10]] = (v[2] === 'lose') ? -v[3] : +v[3], map;
       }, {})
-    , i     = function (a, b) {
-        return (input[a + b] || 0) + input[b + a] || 0;
-      }
-    , sit   = function (from, keys, sum, start, d) {
-        start      = start || from;
-        var result = _.map(keys, function (key) {
-          return _.size(d = _.difference(keys, [key])) ? sit(key, d, sum + i(key, from), start) : sum + i(key, from) + i(key, start);
+    , i     = function (a, b) { return (input[a + b] || 0) + input[b + a] || 0; }
+    , sit   = function (keys) { return _.max(_.flatten(loop(keys[0], keys[0], keys.slice(1), 0))); }
+    , loop  = function (from, start, keys, sum, d) {
+        return _.map(keys, function (key) {
+          return _.size(d = _.difference(keys, [key])) ? loop(key, start, d, sum + i(key, from)) : sum + i(key, from) + i(key, start);
         });
-        return sum ? result : _.max(_.flatten(result));
       }
     ;
 
   return {
 
-    one: sit(keys[0], keys.slice(1), 0),
+    one: sit(keys),
 
-    two: sit('Me', keys, 0)
+    two: sit(['Me'].concat(keys))
 
   };
 
