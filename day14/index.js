5ff18a45d1f11294f1a33f2fd464f1dcc40983c0
hsz <jakub@chrzanowski.info>
2015-12-15 00:28:02 +0100

    day 14 (nodejs) cleanup

--- day14/index.js
+++ day14/index.js
@@ -5,19 +5,15 @@ var _  = require('underscore')
 module.exports = function () {
 
   var time   = 2503
+    , goDeer = function (v, t) { return (t / v[4] | 0) * v[1] * v[2] + _.min([t % v[4], v[2]]) * v[1]; }
     , input  = fs.readFileSync('input.txt', 'utf8').split('\n').map(function (v) {
         return [(v = v.split(' '))[0], +v[3], +v[6], +v[13], +v[6] + +v[13]];
       })
-    , goDeer = function (v, t) {
-        return (t / v[4] | 0) * v[1] * v[2] + _.min([t % v[4], v[2]]) * v[1];
-      }
     ;
 
   return {
 
-    one: _.max(input.map(function (v) {
-      return goDeer(v, time);
-    })),
+    one: _.max(input.map(function (v) { return goDeer(v, time); })),
 
     two: _.max(_.range(1, time).reduce(function (o, t, z) {
       return (z = _.chain(input).map(function (v) { return goDeer(v, t); }))
