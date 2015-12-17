b149348ff66881bb53fd4b41842621cf97fd8f19
hsz <jakub@chrzanowski.info>
2015-12-17 13:13:12 +0100

    day 15 (nodejs) cleanup

--- day15/index.js
+++ day15/index.js
@@ -1,43 +1,31 @@
 'use strict';
 var _  = require('lodash')
-  , co = require('co')
   , fs = require('fs')
   ;
 
 module.exports = function () {
 
-  var input = fs.readFileSync('input.txt', 'utf8').split('\n').map(function (v) {
-        return v.match(/-?\d+/g);
-      })
-    , n     = 100
+  var n     = 100
+    , input = fs.readFileSync('input.txt', 'utf8').split('\n').map(function (v) { return v.match(/-?\d+/g); })
     , comb  = function* (k, m, v, nm) {
         k--, nm = [], m = m || _.range(n + 1);
-        for (let i of m) {
-          if (k !== 1) for (let j = 0; j <= n - _.sum(i); ++j) nm.push([j].concat(i));
-          else yield [n - _.sum(i)].concat(i);
-        }
+        for (let i of m) if (k !== 1) for (let j = 0; j <= n - _.sum(i); ++j) nm.push([j].concat(i)); else yield [n - _.sum(i)].concat(i);
         k && (yield * comb(k, nm));
       }
-    , fn    = function (calories) {
-        var c, it = comb(_.size(input)), result = [];
-        while (c = it.next(), !c.done) {
-          var sums = [0, 0, 0, 0], cals = 0;
-          _.map(input, function (v, k) {
-            cals += +v[4] * c.value[k];
-            for (let i = 0; i < 4; ++i) sums[i] += +v[i] * c.value[k];
-          });
-          if (calories && calories !== cals) continue;
-          result.push(sums.reduce(function (s, v) { return s * (v < 0 ? 0 : v); }, 1));
+    , fn    = function* (cal) {
+        for (const c of comb(_.size(input))) {
+          var sums = [0, 0, 0, 0, 0];
+          _.each(input, function (v, k) { for (let i = 0; i < 5; ++i) sums[i] += +v[i] * c[k]; });
+          (!cal || cal == sums[4]) && (yield _.initial(sums).reduce(function (s, v) { return s * (v < 0 ? 0 : v); }, 1));
         }
-        return _.max(result);
       }
     ;
 
   return {
 
-    one: fn(),
+    one: _.max(Array.from(fn())),
 
-    two: fn(500)
+    two: _.max(Array.from(fn(500)))
 
   };
 
